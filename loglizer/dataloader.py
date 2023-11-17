095bb67fb495430925dd44df4d1b7493a91b53ee
Doris Zhou <zhoudoris13@gmail.com>
2023-11-16 21:21:11 -0500

    Add function to load openstack dataset

--- loglizer/dataloader.py
+++ loglizer/dataloader.py
@@ -142,6 +142,93 @@ def load_HDFS(log_file, label_file=None, window='session', train_ratio=0.5, spli
 
     return (x_train, y_train), (x_test, y_test)
 
+def load_OpenStack(log_file, label_file=None, window='session', train_ratio=0.5, split_type='sequential', save_csv=False, window_size=0):
+    """ Load HDFS structured log into train and test data
+
+    Arguments
+    ---------
+        log_file: str, the file path of structured log.
+        label_file: str, the file path of anomaly labels, None for unlabeled data
+        window: str, the window options including `session` (default).
+        train_ratio: float, the ratio of training data for train/test split.
+        split_type: `uniform` or `sequential`, which determines how to split dataset. `uniform` means
+            to split positive samples and negative samples equally when setting label_file. `sequential`
+            means to split the data sequentially without label_file. That is, the first part is for training,
+            while the second part is for testing.
+
+    Returns
+    -------
+        (x_train, y_train): the training data
+        (x_test, y_test): the testing data
+    """
+
+    print('====== Input data summary ======')
+    if log_file.endswith('.csv'):
+        assert window == 'session', "Only window=session is supported for OpenStack dataset."
+        print("Loading", log_file)
+        struct_log = pd.read_csv(log_file, engine='c',
+                na_filter=False, memory_map=True)
+        data_dict = OrderedDict()
+        for idx, row in struct_log.iterrows():
+            datetime = re.search(r'(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})', row['Content'])
+            
+            data_dict[datetime].append(row['EventId'])
+        data_df = pd.DataFrame(list(data_dict.items()), columns=['DateTime', 'EventSequence'])
+        
+        if label_file:
+            # Split training and validation set in a class-uniform way
+            label_data = pd.read_csv(label_file, engine='c', na_filter=False, memory_map=True)
+            label_data = label_data.set_index('DateTime')
+            label_dict = label_data['Label'].to_dict()
+            data_df['Label'] = data_df['DateTime'].apply(lambda x: 1 if label_dict[x] == 'Anomaly' else 0)
+
+            # Split train and test data
+            (x_train, y_train), (x_test, y_test) = _split_data(data_df['EventSequence'].values, 
+                data_df['Label'].values, train_ratio, split_type)
+        
+            print(y_train.sum(), y_test.sum())
+
+        if save_csv:
+            data_df.to_csv('data_instances.csv', index=False)
+
+        if window_size > 0:
+            x_train, window_y_train, y_train = slice_hdfs(x_train, y_train, window_size)
+            x_test, window_y_test, y_test = slice_hdfs(x_test, y_test, window_size)
+            log = "{} {} windows ({}/{} anomaly), {}/{} normal"
+            print(log.format("Train:", x_train.shape[0], y_train.sum(), y_train.shape[0], (1-y_train).sum(), y_train.shape[0]))
+            print(log.format("Test:", x_test.shape[0], y_test.sum(), y_test.shape[0], (1-y_test).sum(), y_test.shape[0]))
+            return (x_train, window_y_train, y_train), (x_test, window_y_test, y_test)
+
+        if label_file is None:
+            if split_type == 'uniform':
+                split_type = 'sequential'
+                print('Warning: Only split_type=sequential is supported \
+                if label_file=None.'.format(split_type))
+            # Split training and validation set sequentially
+            x_data = data_df['EventSequence'].values
+            (x_train, _), (x_test, _) = _split_data(x_data, train_ratio=train_ratio, split_type=split_type)
+            print('Total: {} instances, train: {} instances, test: {} instances'.format(
+                  x_data.shape[0], x_train.shape[0], x_test.shape[0]))
+            return (x_train, None), (x_test, None), data_df
+    else:
+        raise NotImplementedError('load_OpenStack() only support csv files!')
+
+    num_train = x_train.shape[0]
+    num_test = x_test.shape[0]
+    num_total = num_train + num_test
+    num_train_pos = sum(y_train)
+    num_test_pos = sum(y_test)
+    num_pos = num_train_pos + num_test_pos
+
+    print('Total: {} instances, {} anomaly, {} normal' \
+          .format(num_total, num_pos, num_total - num_pos))
+    print('Train: {} instances, {} anomaly, {} normal' \
+          .format(num_train, num_train_pos, num_train - num_train_pos))
+    print('Test: {} instances, {} anomaly, {} normal\n' \
+          .format(num_test, num_test_pos, num_test - num_test_pos))
+
+    return (x_train, y_train), (x_test, y_test)
+
 def slice_hdfs(x, y, window_size):
     results_data = []
     print("Slicing {} sessions, with window {}".format(x.shape[0], window_size))
